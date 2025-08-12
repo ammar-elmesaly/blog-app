b8fe25488ecbf20e7c8c5bf67e67820ac80d3058
ammar-elmesaly <amarmazn4@gmail.com>
2025-08-12 15:56:52 +0300

    Add basic login and signup pages

--- backend/app.js
+++ backend/app.js
@@ -3,9 +3,12 @@ const express = require('express');
 const app = express();
 const cors = require('cors');
 const path = require('path');
+const bodyParser = require('body-parser');
+
 const PORT = process.env.PORT || 3000;
 
 const loginRouter = require('./routers/login');
+const signupRouter = require('./routers/signup');
 
 // const mongoose = require('mongoose');
 
@@ -14,19 +17,18 @@ const loginRouter = require('./routers/login');
 /* Pug template */
 
 app.set('view engine', 'pug');
-app.set('views','./views');
+app.set('views', path.join(__dirname, 'views'));
 
 app.use(cors());
 app.use('/public', express.static(path.join(__dirname, '../public')));
+app.use(bodyParser.urlencoded({extended: false}));
 
 app.get('/', (req, res) => {
-  res.render('pages/home.pug');
+  res.render('pages/home.pug', {currentPage: 'home'});
 });
 
 app.get('/about', (req, res) => {
-  res.send({
-    message: "Hi from about"
-  });
+  res.render('pages/about.pug', {currentPage: 'about'});
 });
 
 app.get('/home', (req, res) => {
@@ -34,6 +36,8 @@ app.get('/home', (req, res) => {
 });
 
 app.use('/login', loginRouter);
+
+app.use('/signup', signupRouter);
 
 app.get('/{*any}', (req, res) => {  // This must be the last router. don't add any routers below it
   res.sendFile(path.join(__dirname, '../public/pages/404.html'));
--- backend/routers/login.js
+++ backend/routers/login.js
@@ -2,9 +2,11 @@ const express = require('express');
 const router = express.Router();
 
 router.get('/', (req, res) => {
-  res.send({
-    message: "Hi from login"
-  });
+  res.render('pages/login', {currentPage: 'login'});
 });
 
+router.post('/', (req, res) => {
+  console.log(req.body)
+  res.send({user: req.body.user, pass: req.body.password});
+});
 module.exports = router;
